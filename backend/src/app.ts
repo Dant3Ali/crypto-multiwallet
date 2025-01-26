ff427ceecae4b9f12456daa2d2bd6ed628bc3ca3
Doryush Hodzhaev <rabtubr1@gmail.com>
2025-01-26 13:56:14 +0300

    fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,14 @@ import walletRoutes from './routes/wallet'
 import {connectDB} from "./config/db.config";
 import auth from "./routes/authorization";
 
-const app = new Koa();
+async function start () {
+    const app = new Koa();
+    await connectDB();
+    app.use(bodyParser());
+    app.use(walletRoutes.routes());
+    app.use(auth.routes());
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
-connectDB();
-app.use(bodyParser());
-app.use(walletRoutes.routes());
-app.use(auth.routes());
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+start();
